52684f32030f462fd057a6dc0105709a6b34d317
andrey <scream123456@yandex.ru>
2024-04-27 03:22:52 +0500

    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
    An input string is valid if:
    
    Open brackets must be closed by the same type of brackets.
    Open brackets must be closed in the correct order.
    Every close bracket has a corresponding open bracket of the same type.
    
    Example 1:
    
    Input: s = "()"
    Output: true
    Example 2:
    
    Input: s = "()[]{}"
    Output: true
    Example 3:
    
    Input: s = "(]"
    Output: false
    
    Constraints:
    
    1 <= s.length <= 104
    s consists of parentheses only '()[]{}'.

--- src/main/java/com/kamenskiy/io/ExStack.java
+++ src/main/java/com/kamenskiy/io/ExStack.java
@@ -1,16 +1,52 @@
 package com.kamenskiy.io;
 
 import java.util.Stack;
-/*
 
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
  */
 public class ExStack {
     public static void main(String[] args) {
 
+        boolean valid = isValid("[]");
 
+        System.out.println(valid);
     }
 
-    public  boolean isValid(String s) {
-return true;
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (!stack.empty() && stack.peek() == s.charAt(i)) {
+                stack.pop();
+            } else return false;
+        }
+        return stack.isEmpty();
     }
 }
